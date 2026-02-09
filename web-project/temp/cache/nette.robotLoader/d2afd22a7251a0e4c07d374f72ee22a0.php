6edc0edbc02e22c7c57166d7dd5ccdf3a71ea3ed
afranat <frantisek.mrtvy@gmail.com>
2026-02-09 16:33:14 +0100

    Úvodní commit

---
+++ web-project/temp/cache/nette.robotLoader/d2afd22a7251a0e4c07d374f72ee22a0.php
@@ -0,0 +1,45 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Core/RouterFactory.php',
+      1 => 1767892983,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Bootstrap.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Home/HomePresenter.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1767892983,
+    ),
+  ),
+  1 => 
+  array (
+    'null' => 1,
+    'App\\Presentation\\Home\\HomeDefaultTemplate' => 1,
+    'App\\Presentation\\Home\\HomeTemplate' => 1,
+  ),
+  2 => 
+  array (
+  ),
+);
---
+++ web-project/temp/cache/nette.search/d2afd22a7251a0e4c07d374f72ee22a0.php
@@ -0,0 +1,42 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Core/RouterFactory.php',
+      1 => 1767892983,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Bootstrap.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Home/HomePresenter.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1767892983,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/collector/web-project/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1767892983,
+    ),
+  ),
+  1 => 
+  array (
+  ),
+  2 => 
+  array (
+  ),
+);
